9781d0b216fe2e3202ad497bf34a046bfd374920
Sampurna Chatterjee <144989763+GrilledCheese17@users.noreply.github.com>
2024-01-30 20:24:47 +0530

    Update SpringForexApplication.java

--- src/main/java/com/sampurna/forex/SpringForexApplication.java
+++ src/main/java/com/sampurna/forex/SpringForexApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class SpringBootMicroserviceForexServiceApplication {
+public class SpringBootMicroserviceForexServiceApplication throws IOException {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMicroserviceForexServiceApplication.class, args);
